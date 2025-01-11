13429c4fd6c56a7040bfa889a578a000034192bc
TarunSharma-GM <59974666+TarunSharma-GM@users.noreply.github.com>
2025-01-11 14:15:26 +0530

    created using react
    my first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import React, { useState } from 'react';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+function App() {
+  const [mode,setMode] = useState("light");
+  const[alert,setAlert] = useState(null);
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor='#4345';
+      showAlert("Dark Mode Enabled Dosto","success")
+      document.title = "TextUtils - Dark Mode";
+      // Flickering title tab
+      // setInterval(()=>{
+      //   document.title ="TextUtil is Amazing";
+      // },2000);
+      // setInterval(()=>{
+      //   document.title ="TextUtil is Dosto";
+      // },1000);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode Enabled Dosto","success")
+      document.title = "TextUtils - Light Mode";
+    }
+  }
+
+  return (
+    <>
+    {/* <Router> */}
+      {/* <Navbar title="TextUtils" abouttext="About Us" mode={mode} toggleMode={toggleMode}/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+        {/* <Routes> */}
+              {/* <Route exact path="/about" element={<About/>} /> */}
+              {/* <Route exact path="/" element ={<TextForm heading="Enter Text to analyze below" mode={mode} showAlert={showAlert}/>}/> */}
+              <TextForm heading="Enter Text to analyze below" mode={mode} showAlert={showAlert}/>
+        {/* </Routes> */}
+      </div>
+    {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Alert(props) {
+    const capt = (word) => {
+        let txt = word.toLowerCase();
+        return txt.charAt(0).toUpperCase()+txt.slice(1);
+    }
+  return (
+    props.alert && <div>
+        <div className={`alert alert-${props.alert.type}`} role="alert">
+            <strong>{capt(props.alert.type)} </strong>{props.alert.msg}
+        </div>
+    </div>
+  )
+}
