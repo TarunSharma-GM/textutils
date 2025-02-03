96dcf2ea943891d3daa9380f93b1d1475da78a17
TarunSharma-GM <59974666+TarunSharma-GM@users.noreply.github.com>
2025-02-03 22:40:58 +0530

    updated for gh-pages
    optimized for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import './App.css';
 import React, { useState } from 'react';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
-  const [mode,setMode] = useState("light");
-  const[alert,setAlert] = useState(null);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
 
   const toggleMode = () => {
-    if(mode === 'light')
-    {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='#4345';
-      showAlert("Dark Mode Enabled Dosto","success")
+      document.body.style.backgroundColor = '#4345';
+      showAlert("Dark Mode Enabled Dosto", "success")
       document.title = "TextUtils - Dark Mode";
       // Flickering title tab
       // setInterval(()=>{
@@ -39,27 +38,28 @@ function App() {
       //   document.title ="TextUtil is Dosto";
       // },1000);
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode Enabled Dosto","success")
+      showAlert("Light Mode Enabled Dosto", "success")
       document.title = "TextUtils - Light Mode";
     }
   }
 
   return (
     <>
-    <Router>
-      <Navbar title="TextUtils" abouttext="About Us" mode={mode} toggleMode={toggleMode}/>
+      {/* <Router> */}
+      <Navbar title="TextUtils" abouttext="About Us" mode={mode} toggleMode={toggleMode} />
       {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/> */}
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <Routes>
-              <Route exact path="/about" element={<About mode={mode}/>} />
-              <Route exact path="/" element ={<TextForm heading="Enter Text to analyze below" mode={mode} showAlert={showAlert}/>}/>
-        </Routes>
+        <TextForm heading="Enter Text to analyze below" mode={mode} showAlert={showAlert} />
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About mode={mode}/>} /> */}
+        {/* <Route exact path="/" element ={<TextForm heading="Enter Text to analyze below" mode={mode} showAlert={showAlert}/>}/> */}
+        {/* </Routes> */}
       </div>
-    </Router>
+      {/* </Router> */}
     </>
   );
 }
